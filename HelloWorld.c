0fb24cf75dc7c1761dacb22661d8c2f0ff8066e1
Kurt Dudek <kpdudek2@gmail.com>
2019-01-27 12:59:01 -0500

    Hello World in C

---
+++ HelloWorld.c
@@ -0,0 +1,41 @@
+// This is the hello world in C for EK340
+
+#include <stdio.h>
+
+void main() {
+for(int i=0; i<5; i=i+1){ 
+	printf("Hello World\n");
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
