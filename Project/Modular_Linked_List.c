a87b0837399571315e0a388b8c01816eb87979cb
Kurt <kpdudek2@gmail.com>
2019-04-25 23:08:47 -0400

    Requirements finished

--- Project/Modular_Linked_List.c
+++ Project/Modular_Linked_List.c
@@ -36,6 +36,8 @@ int num_elems(elementptr); // Count and print the number of elements
 void test_it(elementptr, elementptr); // traverse and print, then print first and last
 elementptr get_currentptr(elementptr,int); // set current pointer to a random element in list
 void append_to_current(elementptr *,elementptr *,elementptr *); // append an element to current pointer
+void add_before_current(elementptr *,elementptr *,elementptr *); // add an element before current pointer
+void delete_current(elementptr *,elementptr *,elementptr *,int); // deletes current pointer
 void print_menu();
 
 /*
@@ -67,6 +69,8 @@ int main()
     printf("5 - Count and print number of elements\n");
     printf("6 - Append an element to end of list\n");
     printf("7 - Append to current pointer\n");
+    printf("8 - Add an element before the current pointer\n");
+    printf("9 - Deletes the current pointer's element\n");
     printf("0 - Exit\n");
 
     // Print the command line indicator
@@ -90,29 +94,35 @@ int main()
 
           // Traverse and print the linked list
           case 2:
-            if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
+            //if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
             trav_and_print(first);
             break;
 
+          // Test the integrity of the linked list
           case 3:
-            if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
+            //if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
             test_it(first,last);
             break;
 
+          // Set a random current pointer
           case 4:
-            if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
+            //if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
             current = get_currentptr(first,num_elems(first));
-            printf("Current pointer set.\n");
+            if (current != NULL)
+            {
+              printf("Current pointer set.\n");
+            }
             break;
 
+          // Count the number of elements
           case 5:
-            if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
+            //if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
             num_elem = num_elems(first);
             if(num_elem==1)
             {
               printf("There is 1 element\n");
             }
-            else
+            else if (num_elem > 1)
             {
               printf("There are %d elements\n",num_elem);
             }
@@ -120,12 +130,13 @@ int main()
 
           // Append an element to the end of the linked list
           case 6:
-            if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
+            //if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
             addone(&last);
             break;
 
+          // Append an element after the current pointer
           case 7:
-            if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
+            //if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
             if (current == NULL)
             {
               current = get_currentptr(first,num_elems(first));
@@ -134,12 +145,34 @@ int main()
             append_to_current(&first,&current,&last);
             break;
 
-          // Exit
-          case 0:
-            exit = 1;
+          // Add an element before the current pointer
+          case 8:
+            //if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
+            if (current == NULL)
+            {
+              current = get_currentptr(first,num_elems(first));
+              printf(ANSI_COLOR_CYAN"Current pointer did not exist! One was created for you.\n");
+            }
+            add_before_current(&first,&current,&last);
             break;
 
-          // Handle any characters not offered in menu
+          // Delete the element that the current pointer points to
+          case 9:
+            //if (!isLinkedList){printf(ANSI_COLOR_RED"Initialize the linked list!\n");break;}
+            if (current == NULL)
+            {
+              current = get_currentptr(first,num_elems(first));
+              printf(ANSI_COLOR_CYAN"Current pointer did not exist! One was created for you.\n");
+            }
+            delete_current(&first,&current,&last,num_elems(first));
+            break;
+
+          // Exit the program
+          case 0:
+            exit = 1; // set exit flag to true
+            break;
+
+          // Handle any cases not offered in menu
           default:
             printf(ANSI_COLOR_RED"Not a valid option. Please re-enter command.\n\n"ANSI_COLOR_RESET);
             print_menu();
@@ -162,6 +195,22 @@ int main()
 ---> Program functions
 */
 
+// Print the menu options
+void print_menu()
+{
+  printf("The options are:\n");
+  printf("1 - Initialize linked list\n");
+  printf("2 - Traverse and print\n");
+  printf("3 - Test the linked list\n");
+  printf("4 - Randomize current pointer\n");
+  printf("5 - Count and print number of elements\n");
+  printf("6 - Append an element to end of list\n");
+  printf("7 - Append to current pointer\n");
+  printf("8 - Add an element before the current pointer\n");
+  printf("9 - Deletes the current pointer's element\n");
+  printf("0 - Exit\n");
+}
+
 // Set up linked list with one element
 void init(elementptr *f, elementptr *l)
 {
@@ -175,11 +224,17 @@ void init(elementptr *f, elementptr *l)
 // Add element to the end of the linked list
 void addone(elementptr *l)
 {
-    (*l) -> next = (elementptr) malloc(sizeof(element));
-    *l = (*l) -> next;
-    (*l) -> data = rand()%MX+1;
-    (*l) -> next = NULL;
-
+    if (*l == NULL)
+    {
+      printf("There is no linked list!\n");
+    }
+    else
+    {
+      (*l) -> next = (elementptr) malloc(sizeof(element));
+      *l = (*l) -> next;
+      (*l) -> data = rand()%MX+1;
+      (*l) -> next = NULL;
+  }
 }
 
 // Traverse the linked list and print data member
@@ -206,14 +261,19 @@ int num_elems(elementptr f)
 
     current = f;
     if (current == NULL)
+    {
         printf("There is no linked list!\n");
+        return 0;
+    }
     else
+    {
         while (current != NULL)
         {
             num++;
             current = current -> next;
         }
         return num;
+    }
 }
 
 // Call traverse and print. Then print the data value in the first and last
@@ -222,57 +282,134 @@ void test_it(elementptr f,elementptr l)
 {
   trav_and_print(f);
   printf("\n");
-  printf("The first data value is %d\n",f->data);
-  printf("The last data value is %d\n",l->data);
+  if (f != NULL)
+  {
+    printf("The first data value is %d\n",f->data);
+    printf("The last data value is %d\n",l->data);
+  }
 }
 
 // Generates a random number in the range of the length of the linked list and
 // and then assigns a pointer to that index in the linked list
 elementptr get_currentptr(elementptr f, int length)
 {
-  int rand_val = rand()%length,
-      i=0;
-  elementptr current = f;
-
-  while (i < rand_val)
+  if (f == NULL)
   {
-    current = current -> next;
-    i++;
+    printf("There is no linked list!\n");
+    return NULL;
   }
-  return current;
+  else
+  {
+    int rand_val = rand()%length,
+        i=0;
+    elementptr current = f;
+
+    while (i < rand_val)
+    {
+      current = current -> next;
+      i++;
+    }
+    printf("The current pointer points to element #%d\n",i+1);
+    return current;
+  }
 }
 
-// This function adds an element to the linked list after the current Pointer
+// This function adds an element to the linked list after the current pointer
 void append_to_current(elementptr *f, elementptr *c, elementptr *l)
 {
-  elementptr temp;
-  temp = (elementptr)malloc(sizeof(element));
-  temp->data = rand()%MX+1;
-
-  if ((*c)->next == NULL)
+  if (*f == NULL)
   {
-    *l = temp;
+    printf("There is no linked list\n");
   }
-  // if ((*c)->next == NULL)
-  // {
-  //   temp -> next = NULL;
-  //   (*l) = temp;
-  //   (*c) -> next = temp;
-  // }
-  temp->next = (*c)->next;
-  (*c)->next = temp;
-//}
+  else
+  {
+    elementptr temp;
+    temp = (elementptr)malloc(sizeof(element));
+    temp->data = rand()%MX+1;
+
+    if ((*c)->next == NULL)
+    {
+      *l = temp;
+    }
+
+    temp->next = (*c)->next;
+    (*c)->next = temp;
+  }
 }
 
-void print_menu()
+// This function adds an element before the current pointer
+void add_before_current(elementptr *f, elementptr *c, elementptr *l)
 {
-  printf("The options are:\n");
-  printf("1 - Initialize linked list\n");
-  printf("2 - Traverse and print\n");
-  printf("3 - Test the linked list\n");
-  printf("4 - Randomize current pointer\n");
-  printf("5 - Count and print number of elements\n");
-  printf("6 - Append an element to end of list\n");
-  printf("7 - Append to current pointer\n");
-  printf("0 - Exit\n");
+  if (*f == NULL)
+  {
+    printf("There is no linked list\n");
+  }
+  else
+  {
+    elementptr temp, running = *f;
+    temp = (elementptr)malloc(sizeof(element));
+    temp->data = rand()%MX+1;
+
+    if ((*f) == (*c))
+    {
+      (*f) = temp;
+      temp->next = (*c);
+      return;
+    }
+
+    while(running->next !=  (*c))
+    {
+      running = running -> next;
+    }
+
+    running->next = temp;
+    temp->next = (*c);
+  }
+}
+
+// This function deletes the element that the current pointer points to
+void delete_current(elementptr *f, elementptr *c, elementptr *l, int num_elem)
+{
+  if (*f == NULL)
+  {
+    printf("There is no linked list\n");
+  }
+  else
+  {
+    elementptr running = *f;
+    if ((*f) == (*c))
+    {
+      if (num_elem ==1)
+      {
+        free(*f);
+        (*f) = NULL;
+        (*l) = NULL;
+        (*c) = NULL;
+      }
+      else
+      {
+        (*f) = (*c)->next;
+        free(*c);
+        (*c) = NULL;
+        return;
+      }
+    }
+
+    while(running->next !=  (*c))
+    {
+      running = running -> next;
+    }
+
+    if ((*l) == (*c))
+    {
+      (*l) = running;
+      free(*c);
+      (*c) = NULL;
+      return;
+    }
+
+    running -> next = (*c) -> next;
+    free(*c);
+    (*c) = NULL;
+  }
 }
